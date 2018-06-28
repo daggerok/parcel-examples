638311dbb515d239aecd0a45281f72380cfd3d86
Maksim Kostromin <daggerok@gmail.com>
2018-06-28 11:53:23 +0300

    more ignored words (C) whatthecommit.com

---
+++ basic/src/main.js
@@ -0,0 +1,6 @@
+(function main() {
+  'use strict';
+  document.addEventListener('DOMContentLoaded', function onDOMContentLoaded() {
+    document.querySelector('#app').textContent = 'hey!';
+  }, false);
+})();
