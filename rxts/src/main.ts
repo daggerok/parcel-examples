da5784a73c70a8854ae85cb8084070e97b73efc2
Maksim Kostromin <daggerok@gmail.com>
2018-07-20 01:46:27 +0300

    pgsql is being a pain (C) whatthecommit.com

--- rxts/src/main.ts
+++ rxts/src/main.ts
@@ -2,7 +2,7 @@ import { fromEvent } from 'rxjs';
 
 fromEvent(document, 'DOMContentLoaded').subscribe((e: Event) => {
   const button: HTMLButtonElement = document.querySelector('#greet');
-  fromEvent(button, 'click').subscribe(e => {
+  fromEvent(button, 'click').subscribe((e: Event) => {
     document.querySelector('#message').innerHTML = `
       <h1>Hey! 📦 🚀</h1>
     `;
--- ts/src/main.ts
+++ ts/src/main.ts
@@ -1,3 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('#app').innerHTML = '<h1>Hey! 📦 🚀</h1>';
+document.addEventListener('DOMContentLoaded', (e: Event) => {
+  const app: HTMLDivElement = document.querySelector('#app');
+  app.innerHTML = `
+    <h1>Hey! 📦 🚀</h1>
+  `;
+  console.log(`event ${e.type} handled`);
 }, false);
