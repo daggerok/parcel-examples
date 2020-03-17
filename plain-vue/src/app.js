be2b6c939913e54ecef5d58462d1fbbecd0ca3ad
Maksim Kostromin <daggerok@gmail.com>
2020-03-17 03:28:24 +0200

    NOJIRA: No cry (C) whatthecommit.com

---
+++ plain-vue/src/app.js
@@ -0,0 +1,15 @@
+// const Vue = window.Vue;
+// should be already available...
+const app = new Vue({
+  el: '#app',
+  data: {
+    product: {
+      name: 'Socks',
+      // image: '/assets/vmSocks-green-onWhite.jpg',
+      image: './assets/vmSocks-green-onWhite.jpg',
+      quantity: 100,
+    },
+  },
+});
+
+console.log('see: https://vuemastery.com/courses/intro-to-vue-js');
