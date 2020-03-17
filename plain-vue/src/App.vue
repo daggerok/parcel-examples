6e7cbf17585b60ebc49c55be8bd02da56e600384
Maksim Kostromin <daggerok@gmail.com>
2020-03-17 02:08:32 +0200

    Add plain vue

---
+++ plain-vue/src/App.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div>
+    <nav>my app</nav>
+    <div id="app">
+      <h1>{{ message }}</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+  export default {
+    name: 'app',
+    data: () => ({
+      message: 'Hey! 📦 🚀',
+    }),
+  }
+</script>
+
+<style lang="scss">
+  @import "./assets/global.scss";
+</style>
